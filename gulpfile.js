7a66ad60074e5dffe7cd3342e13f4c492967cbbd
Nora Peters <norapeters5@gmail.com>
2017-10-09 20:38:29 -0400

    gulp file and org system

---
+++ gulpfile.js
@@ -0,0 +1,6 @@
+//importing a package/library called gulp, and assigning it to a variable called gulp
+const gulp = require("gulp");
+
+gulp.task("hello", function() {
+  console.log("Hello everyone!");
+});
